7a1a1af71771e9031e518b32a03a9f20484328b1
Jacob Bijsterbosch <info@jacob-bijsterbosch.nl>
2024-09-06 13:54:39 +0200

    Remove test-load-grid-and-nature

--- source/database/build-nature/src/data/sql/load.rb
+++ source/database/build-nature/src/data/sql/load.rb
@@ -2,10 +2,7 @@ add_build_constants
 
 cluster_tables
 
-# Uncomment the (single) test you want to run.
-# Due to the `recurring or circular {import_common} detection` we cannot run all test during one single build.
-run_sql "test_grid_and_nature.sql"
-# run_sql "test_build_nature.sql"
+run_sql "build_base_24.sql"
 
 synchronize_serials
 
--- source/database/build-nature/src/main/sql/structure.sql
+++ source/database/build-nature/src/main/sql/structure.sql
@@ -6,8 +6,6 @@ CREATE EXTENSION postgis;
 
 {import_common 'database-modules/aerius-general/'}
 
-{import_common 'database-modules/grid/'}
-
 {import_common 'database-modules/nature_areas/'}
 {import_common 'database-modules/nature_habitats_and_species/'}
 {import_common 'database-modules/build_nature/'}
