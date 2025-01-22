40bfcfda319b707942458e9222f659aa6f93b78e
Jacob Bijsterbosch <info@jacob-bijsterbosch.nl>
2025-01-22 11:36:22 +0100

    Convert NL build to UK build

--- source/database/build-nature/src/data/sql/build_dataset_24.sql
+++ source/database/build-nature/src/data/sql/build_dataset_24.sql
@@ -1,5 +1,35 @@
-{import_common_into_schema 'database-modules/nature_areas/dataset_24.sql', 'nature'}
-{import_common_into_schema 'database-modules/nature_habitats_and_species/supplied_24.sql', 'nature'}
+BEGIN; SELECT system.load_table('nature.countries', '{data_folder}/aeriusII/UK/public/countries_20211110.txt'); COMMIT;
+BEGIN; SELECT system.load_table('nature.authorities', '{data_folder}/aeriusII/UK/public/authorities_20220128.txt'); COMMIT;
+
+BEGIN; SELECT system.load_table('nature.natura2000_areas', '{data_folder}/aeriusII/UK/public/natura2000_areas_20240517.txt', TRUE); COMMIT;
+BEGIN; SELECT system.load_table('nature.natura2000_directives', '{data_folder}/aeriusII/UK/public/natura2000_directives_20220408.txt', TRUE); COMMIT;
+BEGIN; SELECT system.load_table('nature.natura2000_directive_areas', '{data_folder}/aeriusII/UK/public/natura2000_directive_areas_20240517.txt', TRUE); COMMIT;
+
+-- UK has no specific natura2000_area_properties, so insert something sensible for now.
+-- definitief is a dutch term, but that's needed due to enum aspect. It ensures that every habitat_area within the natura2000_area is considered relevant.
+BEGIN;
+INSERT INTO nature.natura2000_area_properties (natura2000_area_id, registered_surface, design_status)
+	SELECT natura2000_area_id, ST_Area(geometry), 'definitief' FROM nature.natura2000_areas;
+COMMIT;
+
+BEGIN; SELECT system.load_table('nature.habitat_types', '{data_folder}/aeriusII/UK/public/habitat_types_20240517.txt', TRUE); COMMIT;
+BEGIN; SELECT system.load_table('nature.habitat_type_critical_levels', '{data_folder}/aeriusII/UK/public/habitat_type_critical_levels_20240517.txt', TRUE); COMMIT;
+BEGIN; SELECT system.load_table('nature.habitat_areas', '{data_folder}/aeriusII/UK/public/habitat_areas_20240517.txt', TRUE); COMMIT;
+
+-- UK has no specific habitat relations or properties data either, so insert something sensible for now.
+-- Since we have no other information, use a 1:1 relation between habitat type and goal habitat type
+BEGIN;
+INSERT INTO nature.habitat_type_relations (habitat_type_id, goal_habitat_type_id)
+	SELECT habitat_type_id, habitat_type_id FROM nature.habitat_types;
+COMMIT;
+
+-- All habitat-areas are going to be marked as relevant
+-- (level isn't entirely correct, but can't use 'none' and nothing else really seems to match)
+BEGIN;
+INSERT INTO nature.habitat_properties (goal_habitat_type_id, assessment_area_id, quality_goal, extent_goal, design_status)
+	SELECT DISTINCT habitat_type_id, assessment_area_id, 'level'::nature.habitat_goal_type, 'level'::nature.habitat_goal_type, 'definitief'::nature.design_status_type
+		FROM nature.habitat_areas;
+COMMIT;
 
 {import_common_into_schema 'database-modules/build_nature/build.sql', 'nature'}
 {import_common_into_schema 'database-modules/build_nature/store.sql', 'nature'}
--- source/database/build-nature/src/main/sql/structure.sql
+++ source/database/build-nature/src/main/sql/structure.sql
@@ -11,3 +11,54 @@ CREATE SCHEMA nature;
 {import_common_into_schema 'database-modules/nature_areas/', 'nature'}
 {import_common_into_schema 'database-modules/nature_habitats_and_species/', 'nature'}
 {import_common_into_schema 'database-modules/build_nature/', 'nature'}
+
+-- Override setup.build_relevant_habitat_areas_view to ignore directive and process every area, as UK doesn't differentiate between habitat and species.
+-- Also ignore design status, as that is not something used in UK, so no point in taken it into account
+-- Supplied data contains false for both habitat_directive and bird_directive, but we still want to generate stuff
+CREATE OR REPLACE VIEW nature.build_relevant_habitat_areas_view AS
+WITH natura2000_directive_area_properties AS (
+	SELECT
+		natura2000_directive_area_id,
+		natura2000_area_id AS assessment_area_id,
+		geometry
+
+		FROM nature.natura2000_directive_areas
+)
+SELECT * FROM
+	(SELECT
+		assessment_area_id,
+		habitat_area_id,
+		habitat_type_id,
+		coverage,
+		ST_CollectionExtract(ST_Multi(ST_Union(ST_Intersection(natura2000_directive_area_geometry, habitat_area_geometry))), 3) AS geometry
+
+		FROM
+			-- Nitrogen-sensitive designated habitat within an area
+			(SELECT
+				assessment_area_id,
+				habitat_area_id,
+				habitat_type_id,
+				natura2000_directive_area_id,
+				coverage,
+				natura2000_directive_area_properties.geometry AS natura2000_directive_area_geometry,
+				habitat_areas.geometry AS habitat_area_geometry
+
+				FROM nature.habitat_areas
+					INNER JOIN nature.habitat_types USING (habitat_type_id)
+					INNER JOIN nature.habitat_type_sensitivity_view USING (habitat_type_id)
+					INNER JOIN natura2000_directive_area_properties USING (assessment_area_id)
+					INNER JOIN nature.habitat_type_relations USING (habitat_type_id)
+					LEFT JOIN nature.habitat_properties USING (goal_habitat_type_id, assessment_area_id)
+					LEFT JOIN nature.designated_habitats_view USING (habitat_type_id, assessment_area_id)
+
+				WHERE
+					sensitive IS TRUE
+					AND designated_habitats_view.habitat_type_id IS NOT NULL
+
+		) AS relevant_habitats
+
+		GROUP BY assessment_area_id, habitat_area_id, habitat_type_id, coverage
+	) AS relevant_habitat_areas
+
+	WHERE NOT ST_IsEmpty(geometry)
+;
