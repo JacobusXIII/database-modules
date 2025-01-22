b431082fa59d73e9da3cc9ee654f6985b5412bdd
Jacob Bijsterbosch <info@jacob-bijsterbosch.nl>
2025-01-22 10:45:18 +0100

    Convert two existing views to a materialized view
    Convert habitats and relevant_habitats.
    Generating the data only takes a few minutes and materialized views area much easier to maintain then db-data files solution.
    Edit build_nature as well.

--- source/modules/src/data/sql/database-modules/nature_habitats_and_species/dataset_22.sql
+++ source/modules/src/data/sql/database-modules/nature_habitats_and_species/dataset_22.sql
@@ -2,10 +2,5 @@
 
 -- Generated data
 BEGIN; SELECT system.load_table('relevant_habitat_areas', '{data_folder}/common/nature_habitats_and_species/22/nature.relevant_habitat_areas_20230104.txt', FALSE); COMMIT;
-BEGIN; SELECT system.load_table('habitats', '{data_folder}/common/nature_habitats_and_species/22/nature.habitats_20230104.txt', FALSE); COMMIT;
-BEGIN; SELECT system.load_table('relevant_habitats', '{data_folder}/common/nature_habitats_and_species/22/nature.relevant_habitats_20230104.txt', FALSE); COMMIT;
 
-
--- Refresh all materialized views
-BEGIN; REFRESH MATERIALIZED VIEW relevant_goal_habitats; COMMIT;
-BEGIN; REFRESH MATERIALIZED VIEW relevant_species; COMMIT;
+{import_common 'database-modules/nature_habitats_and_species/refresh_materialized_views.sql'}
