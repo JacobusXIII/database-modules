21b6dc67064684a5df5070cdc45dbc5ed95afcfe
Jacob Bijsterbosch <info@jacob-bijsterbosch.nl>
2025-07-18 09:39:38 +0200

    Add all non m25 fixes again that are removed from the m25 PR

--- source/modules/src/main/sql/database-modules/grid_receptors_to/single-zoom-level.sql
+++ source/modules/src/main/sql/database-modules/grid_receptors_to/single-zoom-level.sql
@@ -58,7 +58,7 @@ SELECT
 	receptor_id,
 	surface * receptor_habitat_coverage AS cartographic_surface
 	
-	FROM grid.receptors_to_critical_deposition_areas
+	FROM receptors_to_critical_deposition_areas
 	
 	WHERE type = 'relevant_habitat'
 ;
