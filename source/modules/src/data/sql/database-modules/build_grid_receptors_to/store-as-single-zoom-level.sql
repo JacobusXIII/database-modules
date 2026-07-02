2a944c1e706c79633230b52782930d0a8d477024
Jacob Bijsterbosch <info@jacob-bijsterbosch.nl>
2026-07-02 16:37:55 +0200

    Add missing schema prefix for store table

--- source/modules/src/data/sql/database-modules/build_grid_receptors_to/store-as-single-zoom-level.sql
+++ source/modules/src/data/sql/database-modules/build_grid_receptors_to/store-as-single-zoom-level.sql
@@ -43,5 +43,5 @@ SELECT system.store_query(
 
 
 -- Store these files for Calculator as well, because it's not using MATERIALIZED VIEWS yet.
-SELECT system.store_table('habitats', '{data_folder}/export/{tablename}_{datesuffix}.txt');
-SELECT system.store_table('relevant_habitats', '{data_folder}/export/{tablename}_{datesuffix}.txt');
+SELECT system.store_table('nature.habitats', '{data_folder}/export/{tablename}_{datesuffix}.txt');
+SELECT system.store_table('nature.relevant_habitats', '{data_folder}/export/{tablename}_{datesuffix}.txt');
