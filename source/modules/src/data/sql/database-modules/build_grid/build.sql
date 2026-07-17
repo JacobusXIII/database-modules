ee87ded8e25f5c0ea7e89f0b78716f4b20795a82
luitvm <38038189+eltee1@users.noreply.github.com>
2026-07-17 12:01:35 +0200

    fix build (wont build without the set_path to grid and public) and add grid-and nature generated import-files without the abroad data loaded.

--- source/modules/src/data/sql/database-modules/build_grid/build.sql
+++ source/modules/src/data/sql/database-modules/build_grid/build.sql
@@ -1,5 +1,7 @@
+SET search_path TO 'grid', 'public';
 SELECT system.raise_notice('Build: geometry_of_interests @ ' || timeofday());
 BEGIN; SELECT grid.ae_build_geometry_of_interests(); COMMIT;
 
+SET search_path TO 'grid', 'public';
 SELECT system.raise_notice('Build: hexagons and receptors @ ' || timeofday());
 BEGIN; SELECT grid.ae_build_hexagons_and_receptors(); COMMIT;
